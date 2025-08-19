b063feddd866a5cddb886b0ec416853e064322c9
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-19 23:47:00 +0900

    feat: Add initial seed data for workouts, exercises, and exercise_sets

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,5 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+workout = Workout.create!(notes: "", performed_on: Date.today)
+exercise = workout.exercises.create!(name: "Assisted Pull-up", notes: "Stretch lats on eccentric")
+exercise.exercise_sets.create!(set_number: 1, weight: 26, rep_count: 10, rir: 0)
+exercise.exercise_sets.create!(set_number: 2, weight: 26, rep_count: 8, rir: 0)
