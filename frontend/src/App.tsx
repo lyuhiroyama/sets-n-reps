17a6ef94a513780baa41afa7b6bd4bcd81415cff
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-24 03:11:04 +0900

    feat(frontend/dashboard): Add Dashboard component - Added Dashboard component to serve as the main UI after sign-in. - Updated routing in App.tsx to include navigation to this Dashboard. - Added a corresponding Dashboard.module.css file.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignIn from "./components/SignIn/SignIn";
+import Dashboard from "./components/Dashboard/Dashboard"
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
                 <header className="App-header">
                     <Routes>
                         <Route path="/" element={<SignIn />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
                     </Routes>
                 </header>
             </div>
