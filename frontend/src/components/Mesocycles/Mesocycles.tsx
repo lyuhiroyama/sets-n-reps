2a62bfc08ed3b58a577dd3c9cf456d8d74c93a7f
lyuhiroyama <lyuhiroyama@gmail.com>
2025-08-31 14:59:04 +0900

    feat(frontend): Add Mesocycles.tsx default display

--- frontend/src/components/Mesocycles/Mesocycles.tsx
+++ frontend/src/components/Mesocycles/Mesocycles.tsx
@@ -1,3 +1,14 @@
+import styles from "./Mesocycles.module.css"
+
 export default function Mesocycles() {
-    return <div>Mesocycles</div>;
+    return (
+        <div className={styles.component}>
+            <div className={styles.container}>
+                <div className={styles.header_container}>
+                    <h1>Mesocycles</h1>
+                    <button>+ NEW</button>
+                </div>
+            </div>
+        </div>
+    );
 }
