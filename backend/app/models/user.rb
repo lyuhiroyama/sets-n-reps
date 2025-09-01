990dda05e2f762348b2a6f4f1729edb9f6c3e92e
lyuhiroyama <lyuhiroyama@gmail.com>
2025-09-01 17:31:44 +0900

    fix(both): Undo uncecessary Devise & CSRF customizations (2) - Forgot to make the following changes, that should have been done in the commit 'fix(both): Undo uncecessary Devise & CSRF customizations' (Commit 484c266): 	- Dropped auth_token column from users table 	- Removed before_create callback & generate_auth_token method from user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,10 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  before_create :generate_auth_token
-
-  def generate_auth_token
-    self.auth_token = SecureRandom.hex(20)
-  end
 end
