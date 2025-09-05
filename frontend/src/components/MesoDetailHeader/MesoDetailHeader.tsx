ae8c9d15413e6baca8d93e5b46c21bdf49e06e03
lyuhiroyama <lyuhiroyama@gmail.com>
2025-09-05 20:33:43 +0900

    feat(frontend): Add MesoDetailHeader.tsx default display - Have yet to add functionality to calendar & settings buttons

---
+++ frontend/src/components/MesoDetailHeader/MesoDetailHeader.tsx
@@ -0,0 +1,35 @@
+import styles from "./MesoDetailHeader.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+
+type MesocycleLite = {
+    name?: string;
+    workouts?: WorkoutLite[]
+}
+
+type WorkoutLite = {
+    day_of_week?: string
+}
+
+export default function MesoDetailHeader({ mesocycle }: { mesocycle?: MesocycleLite }) {
+    const mesoName = mesocycle?.name ?? "Mesocycle";
+    const dayOfWeek = mesocycle?.workouts?.[0]?.day_of_week
+            ? `Week 1・${mesocycle.workouts[0].day_of_week}`
+            : "Week 1";
+
+    return (
+        <div className={styles.component}>
+            <div className={styles.container}>
+                <div className={styles.div_texts}>
+                    <span>{mesoName}</span>
+                    <h3>{dayOfWeek}</h3>
+                </div>
+                <div className={styles.div_icons}>
+                    <FontAwesomeIcon icon={faCalendarDays} />
+                    <FontAwesomeIcon icon={faGear} />
+                </div>
+            </div>
+        </div>
+    )
+}
