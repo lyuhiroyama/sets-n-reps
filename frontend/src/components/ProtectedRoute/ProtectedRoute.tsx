9aaaddabc1c209172bba4258618948e0ac058c13
lyuhiroyama <lyuhiroyama@gmail.com>
2025-09-18 11:48:08 +0900

    feat(frontend): Add loading spinner to ProtectedRoute
    - Replaced the plain white Lodading... text with a spinning green circle with a dark grey background

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
 import { ReactNode } from "react";
 import { useAuth } from "../AuthContext/AuthContext";
 import { Navigate } from "react-router-dom";
+import styles from "./ProtectedRoute.module.css";
 
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
     const { isAuthenticated } = useAuth();
 
     if (isAuthenticated === null) {
-        return <div>Loading...</div>;
+        return (
+            <div className={styles.component}>
+                <div className={styles.spinner} />
+            </div>
+        )
     }
 
     return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
