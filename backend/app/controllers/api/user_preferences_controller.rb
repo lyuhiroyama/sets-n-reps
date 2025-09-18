856e44067ef9c4c3cf00b42523d30ff47d88ae0b
lyuhiroyama <lyuhiroyama@gmail.com>
2025-09-19 06:45:44 +0900

    secure(backend): Require authentication in UserPreferencesController

--- backend/app/controllers/api/user_preferences_controller.rb
+++ backend/app/controllers/api/user_preferences_controller.rb
@@ -1,5 +1,6 @@
 class API::UserPreferencesController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token # For development environment. Remove in production.
+  before_action :authenticate_user!
 
   def update
     if current_user.update(user_preferences_params)
