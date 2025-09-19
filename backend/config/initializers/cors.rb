ed8b6d5e6f86f1ea33f5b8c533c1b680186a69c2
lyuhiroyama <lyuhiroyama@gmail.com>
2025-09-19 11:25:29 +0900

    chore(backend): Restrict CORS to development environment

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,13 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins "http://localhost:3001" # Frontend origin
+# Enable CORS only in development
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "http://localhost:3001" # Frontend origin
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true # To allow cookies to be sent/received
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true # To allow cookies to be sent/received
+    end
   end
 end
