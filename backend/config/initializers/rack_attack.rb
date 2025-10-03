78ece292a37700127ad82534403d09e204d6b4c5
lyuhiroyama <lyuhiroyama@gmail.com>
2025-10-03 12:07:54 +0900

    feat(backend): Implement throttling for "/api/" endpoints - Updated throttling numbers for sign-in & sign-up - Added throttling for routes under the /api/ namespace - Rack::Attack keeps a counter of each IP at 'req.ip'

--- backend/config/initializers/rack_attack.rb
+++ backend/config/initializers/rack_attack.rb
@@ -1,8 +1,20 @@
 if Rails.env.production?
   class Rack::Attack
-    # Throttle sign-in attempts per IP
-    throttle("logins/ip", limit: 5, period: 20.seconds) { |req| req.post? && req.path == "/users/sign_in" && req.ip }
-    # Throttle sign-up attempts per IP
-    throttle("signups/ip", limit: 3, period: 1.minute)  { |req| req.post? && req.path == "/users" && req.ip }
+    # Throttle sign-in attempts (10 attempts per 20 seconds)
+    throttle("logins/ip", limit: 10, period: 20.seconds) { |req| req.post? && req.path == "/users/sign_in" && req.ip }
+    # Throttle sign-up attempts (5 attempts per minute)
+    throttle("signups/ip", limit: 5, period: 1.minute)  { |req| req.post? && req.path == "/users" && req.ip }
+    # Throttle read operations that has 'api' namespaced endpoints (300 requests per 5 mins)
+    throttle("authenticated_api/ip", limit: 300, period: 5.minutes) do |req|
+      if req.path.start_with?("/api/") && req.env['warden'].authenticated?
+        req.ip
+      end
+    end
+    # Throttle write operations that has 'api' namespaced endpoints (300 requests per 5 mins)
+    throttle("authenticated_api_writes/ip", limit: 300, period: 5.minutes) do |req|
+      if req.path.start_with?("/api/") && req.env['warden'].authenticated? && !req.get?
+        req.ip
+      end
+    end
   end
 end
